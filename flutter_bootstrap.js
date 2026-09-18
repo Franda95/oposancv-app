38a73d7811d8bc247b86127a4c7efcd1d5c786a1
Fran <fran@opoimagencv.com>
2026-09-18 11:45:49 +0200

    Enable web premium purchase

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "378740408" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1094576220" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
